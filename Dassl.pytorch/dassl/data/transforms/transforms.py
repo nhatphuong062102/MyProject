319906c1a08de21b5df9b2964c20067a0b490d3a
NhatPhuong2106 <phuongily24@gmail.com>
2026-09-26 20:52:00 +0700

    test tfm 4crop-center

--- Dassl.pytorch/dassl/data/transforms/transforms.py
+++ Dassl.pytorch/dassl/data/transforms/transforms.py
@@ -564,33 +564,33 @@ def _build_transform_train(cfg, choices, target_size, normalize):
 
 
 """Transform tạo 3 CROP và 1 FULL resize thẳng 224 x 224"""
-def _build_transform_test(cfg, choices, target_size, normalize):
-    print("Building transform_test")
-
-    interp_mode = INTERPOLATION_MODES[cfg.INPUT.INTERPOLATION]
-    input_size = cfg.INPUT.SIZE
-
-    print("+ multi-crop (3 crop)")
-    print(f"+ add 1 full image resized to target_size={target_size}")
-    print("+ to torch tensor of range [0, 1]")
-    print(f"+ normalization (mean={cfg.INPUT.PIXEL_MEAN}, std={cfg.INPUT.PIXEL_STD})")
-
-    return MultiCropSquare(size=input_size, interpolation=interp_mode, normalize=normalize, k=3)
-
-
-"""Transform tạo 3 CROP và 1 FULL center-crop """
 # def _build_transform_test(cfg, choices, target_size, normalize):
 #     print("Building transform_test")
 
 #     interp_mode = INTERPOLATION_MODES[cfg.INPUT.INTERPOLATION]
 #     input_size = cfg.INPUT.SIZE
 
-#     print("+ multi-crop (3 crop) + 1 center-crop")
+#     print("+ multi-crop (3 crop)")
+#     print(f"+ add 1 full image resized to target_size={target_size}")
 #     print("+ to torch tensor of range [0, 1]")
-#     print(f"+ normalization (mean={cfg.INPUT.PIXEL_MEAN}, "
-#           f"std={cfg.INPUT.PIXEL_STD})")
+#     print(f"+ normalization (mean={cfg.INPUT.PIXEL_MEAN}, std={cfg.INPUT.PIXEL_STD})")
 
-#     return MultiCropSquareCenter(size=input_size, interpolation=interp_mode, normalize=normalize, k=3)
+#     return MultiCropSquare(size=input_size, interpolation=interp_mode, normalize=normalize, k=3)
+
+
+"""Transform tạo 3 CROP và 1 FULL center-crop """
+def _build_transform_test(cfg, choices, target_size, normalize):
+    print("Building transform_test")
+
+    interp_mode = INTERPOLATION_MODES[cfg.INPUT.INTERPOLATION]
+    input_size = cfg.INPUT.SIZE
+
+    print("+ multi-crop (3 crop) + 1 center-crop")
+    print("+ to torch tensor of range [0, 1]")
+    print(f"+ normalization (mean={cfg.INPUT.PIXEL_MEAN}, "
+          f"std={cfg.INPUT.PIXEL_STD})")
+
+    return MultiCropSquareCenter(size=input_size, interpolation=interp_mode, normalize=normalize, k=3)
 
 
 
